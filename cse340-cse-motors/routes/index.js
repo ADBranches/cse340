3bbd7e9e3cedd6507c3f0377f30793352c61e011
Edwin Kambale <bambiedwins03@gmail.com>
2026-02-04 17:30:31 +0300

    latest changes

--- cse340-cse-motors/routes/index.js
+++ cse340-cse-motors/routes/index.js
@@ -5,7 +5,7 @@ const router = Router();
 
 // Home route - renders CSE Motors home view
 router.get("/", (req, res) => {
-  res.render("index", { title: "CSE Motors" });
+  res.render("index", { title: "Home" });
 });
 
 export default router;
